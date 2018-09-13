a6ddcd8a1a7a20f96e165fe3b63e6d1218f13a01
mandasoftma <mandasoftma@gmail.com>
2018-09-13 00:13:57 -0500

    Commit ajustes notificaciones Push
    Se adjuntan los import de los servicios de OneSignal con integración con firebase.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -21,39 +21,9 @@ import { SplashScreen } from '@ionic-native/splash-screen';
 
 import { HttpClientModule } from '@angular/common/http';
 import { SwebProvider } from '../providers/sweb/sweb';
-
-//import { CloudSettings, CloudModule } from '@ionic/cloud-angular';
-// Pro.init('f60104df', {
-//   appVersion: '0.0.1'
-// })
-
-// @Injectable()
-// export class MyErrorHandler implements ErrorHandler {
-//   ionicErrorHandler: IonicErrorHandler;
-
-//   constructor(injector: Injector) {
-//     try {
-//       this.ionicErrorHandler = injector.get(IonicErrorHandler);
-//     } catch(e) {
-//       // Unable to get the IonicErrorHandler provider, ensure
-//       // IonicErrorHandler has been added to the providers list below
-//     }
-//   }
-
-//   handleError(err: any): void {
-//     Pro.monitoring.handleNewError(err);
-//     // Remove this if you want to disable Ionic's auto exception handling
-//     // in development mode.
-//     this.ionicErrorHandler && this.ionicErrorHandler.handleError(err);
-//   }
-// }
+import { OneSignal } from '@ionic-native/onesignal';
 
 
-// const cloudSettings: CloudSettings = {
-//   'core': {
-//     'app_id': "f60104df"
-//   }
-// };
 
 @NgModule({
   declarations: [
@@ -90,6 +60,7 @@ import { SwebProvider } from '../providers/sweb/sweb';
     SplashScreen,
     SwebProvider,
     IonicErrorHandler,
+    OneSignal,
     {provide: ErrorHandler, useClass: IonicErrorHandler}
     
   ]
